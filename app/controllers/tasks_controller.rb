4ea9198de2749059055b12d7571c539d8790e92b
1alron <roma.niko393@gmail.com>
2026-05-16 21:16:50 +0300

    Added filtration by status and date

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :update, :destroy ]
 
   def index
-    tasks = Task.all
+    tasks = TasksFilterService.new(params).call
     render json: tasks, status: :ok
   end
 
---
+++ app/services/TasksFilterService.rb
@@ -0,0 +1,37 @@
+class TasksFilterService
+  def initialize(params)
+    @params = params
+    @tasks = Task.all
+  end
+
+  def call
+    filter_by_date
+    filter_by_status
+    @tasks
+  end
+
+  private
+
+  def filter_by_date
+    start_date = parse_date(@params[:start_date])
+    end_date = parse_date(@params[:end_date])
+
+    return @tasks = Task.none if (@params[:start_date].present? && start_date.nil?) ||
+      (@params[:end_date].present? && end_date.nil?)
+
+    @tasks = @tasks.where("exec_date >= ?", start_date) if start_date.present?
+    @tasks = @tasks.where("exec_date <= ?", end_date) if end_date.present?
+  end
+
+  def filter_by_status
+    return if @params[:status].blank?
+    @tasks = @tasks.where(status: @params[:status])      
+  end
+
+  def parse_date(date_string)
+    return nil if date_string.blank?
+    DateTime.parse(date_string)
+  rescue ArgumentError, TypeError
+    nil
+  end
+end
