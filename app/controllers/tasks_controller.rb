6a9f35017b29b1de69e83252159306fe0b9047f7
1alron <roma.niko393@gmail.com>
2026-05-16 16:12:46 +0300

    Added CRUD for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,44 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy ]
+
   def index
+    tasks = Task.all
+    render json: tasks, status: :ok
   end
 
   def show
-  end
-
-  def new
+    render json: @task, status: :ok
   end
 
   def create
-  end
-
-  def edit
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @task.update(task_params)
+      render json: @task, status: :ok
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @task.destroy
+    head :no_content     
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :exec_date)
   end
 end
