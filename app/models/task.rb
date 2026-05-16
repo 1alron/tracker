20954c938aeb5d5935ba28bf51e64254eb356b27
1alron <roma.niko393@gmail.com>
2026-05-16 16:27:44 +0300

    Added validation and enum in Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  enum status: [ :new, :in_progress, :done, :cancelled ]
+
+  validates :title, presence: true
+  validates :status, presence: true
+  validates :description, presence: true
+  validates :exec_date, presence: true
 end
