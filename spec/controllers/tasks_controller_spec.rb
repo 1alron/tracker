fd89e8fea16f0dc380a5b5630576570478c44cf0
1alron <roma.niko393@gmail.com>
2026-05-21 21:59:29 +0300

    Added POST #create example tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe TasksController, type: :controller do
     end
     it 'correct answer format by header' do
       get :index
-      expect(response.content_type).to include('application/json')  
+      expect(response.content_type).to include('application/json')
     end
     it 'correct answer charset' do
       get :index
@@ -46,7 +46,29 @@ RSpec.describe TasksController, type: :controller do
 
     it 'returns 404 status code if task not found' do
       get :show, params: { id: 0 }
-      expect(response).to have_http_status(:not_found)  
+      expect(response).to have_http_status(:not_found)
     end
   end
+
+  describe "POST #create" do
+    it 'creates task' do
+      post :create, params: test_task_params
+      expect(Task.first).to have_attributes(title: "test_title")
+    end
+    it 'returns task attributes' do
+      post :create, params: test_task_params
+      expect(JSON.parse(response.body).keys).to include('id', 'title', 'description', 'status', 'exec_date')
+    end
+  end
+end
+
+def test_task_params
+  {
+    task: {
+          title: "test_title",
+          description: "test description",
+          status: "new",
+          exec_date: Date.tomorrow
+        }
+  }
 end
